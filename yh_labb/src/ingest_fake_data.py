094c30bd9c0c96776582cd3514f8cb90290738ae
svartfika <svartfika@proton.me>
2025-04-02 17:26:25 +0200

    initial mapping of tables

--- yh_labb/src/ingest_fake_data.py
+++ yh_labb/src/ingest_fake_data.py
@@ -3,27 +3,37 @@ from pydantic import BaseModel, Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy import URL, create_engine, inspect
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base, AutomapBase
 
 
-class PostgresSettings(BaseModel):
+class PostgresSettings(BaseSettings):
     host: str = Field(default="127.0.0.1")
     port: int = Field(default="5432")
     user: str = Field(default="postgres")
     password: SecretStr = Field(default_factory=lambda: SecretStr(""))
     dbname: str = Field(default="postgres_db")
 
+    # model_config = SettingsConfigDict(env_prefix="POSTGRES_")
+
+
+class Database(BaseSettings):
+    dbschema: str = Field(default="yh")
+
 
 class Settings(BaseSettings):
-    postgres: PostgresSettings
+    postgres: PostgresSettings = Field(default_factory=PostgresSettings)
+    database: Database = Field(default_factory=Database)
+
     model_config = SettingsConfigDict(
         env_file=".env",
-        extra="ignore",
         env_nested_delimiter="_",
         case_sensitive=False,
+        extra="ignore",
     )
 
 
 settings = Settings()
+
 
 postgres_url = URL.create(
     drivername="postgresql+psycopg",
@@ -36,6 +46,40 @@ postgres_url = URL.create(
 
 engine = create_engine(postgres_url)
 
+
+Base: AutomapBase = automap_base()
+
+Base.prepare(autoload_with=engine, schema=settings.database.dbschema)
+
+Person = Base.classes.person
+
+AffiliationRole = Base.classes.affiliation_role
+Affiliation = Base.classes.affiliation
+
+EmploymentCategory = Base.classes.employment_category
+Employment = Base.classes.employment
+Consultant = Base.classes.consultant
+FullTime = Base.classes.full_time
+
+Branch = Base.classes.branch
+Program = Base.classes.program
+
+ModuleType = Base.classes.module_type
+Module = Base.classes.module
+Course = Base.classes.course
+
+Manager = Base.classes.manager
+Teacher = Base.classes.teacher
+Student = Base.classes.student
+Cohort = Base.classes.cohort
+
+ProgramBranch = Base.classes.program_branch
+ModuleProgram = Base.classes.module_program
+CourseTeacher = Base.classes.course_teacher
+CourseStudent = Base.classes.course_student
+CohortManager = Base.classes.cohort_manager
+StudentCohort = Base.classes.student_cohort
+
 with Session(engine) as s:
     inspector = inspect(engine)
-    pprint(inspector.get_table_names("yh"))
+    pprint(inspector.get_table_names(settings.database.dbschema))
