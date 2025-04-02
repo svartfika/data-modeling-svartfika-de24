f9602005cd2c50cd6e2a7281e191797a91cde54a
svartfika <svartfika@proton.me>
2025-04-02 12:05:41 +0200

    added constraints

--- yh_labb/sql/init.sql
+++ yh_labb/sql/init.sql
@@ -61,121 +61,143 @@ CREATE TABLE IF NOT EXISTS yh.full_time (
     full_time_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     employment_id bigint NOT NULL REFERENCES yh.employment (employment_id),
     salary_monthly numeric(10,2),
-    hours_weekly numeric
+    hours_weekly smallint
 ) ;
 
 
 
 CREATE TABLE IF NOT EXISTS yh.branch (
     branch_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR,
-    city VARCHAR,
-    address VARCHAR
+    name varchar(100) NOT NULL,
+    city varchar(100) NOT NULL,
+    address varchar(100),
+    description text,
+    UNIQUE (name, city)
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.program (
     program_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR,
-    code VARCHAR,
-    cycle INTEGER,
-    description TEXT,
-    date_start DATE,
-    date_end DATE    
+    name varchar(100) NOT NULL,
+    code varchar(100) NOT NULL,
+    cycle smallint NOT NULL CHECK (cycle BETWEEN 1 AND 3),
+    description text,
+    date_start date NOT NULL,
+    date_end date,
+    UNIQUE (code, cycle, date_start)
 ) ;
 
 
 
 CREATE TABLE IF NOT EXISTS yh.module_type (
     module_type_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR,
-    description TEXT   
+    name varchar(100) NOT NULL UNIQUE,
+    description text
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.module (
     module_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    module_type_id INTEGER REFERENCES yh.module_type (module_type_id),
-    branch_id INTEGER REFERENCES yh.branch (branch_id),
-    name VARCHAR,
-    code VARCHAR,
-    description TEXT,
-    date_start DATE,
-    date_end DATE      
+    module_type_id bigint NOT NULL REFERENCES yh.module_type (module_type_id),
+    branch_id bigint NOT NULL REFERENCES yh.branch (branch_id),
+    name varchar(100) NOT NULL,
+    code varchar(100) NOT NULL,
+    description text,
+    date_start date NOT NULL,
+    date_end date,
+    UNIQUE (branch_id, code, date_start) 
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.course (
     course_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    module_id INTEGER REFERENCES yh.module (module_id),
-    name VARCHAR,
-    code VARCHAR,
-    credits INTEGER,
-    description TEXT,
-    date_start DATE,
-    date_end DATE   
+    module_id bigint NOT NULL REFERENCES yh.module (module_id),
+    name varchar(100) NOT NULL,
+    code varchar(100) NOT NULL,
+    credits smallint NOT NULL,
+    description text,
+    date_start date NOT NULL,
+    date_end date,
+    UNIQUE (module_id, code, date_start)
 ) ;
 
 
 
 CREATE TABLE IF NOT EXISTS yh.manager (
     manager_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    employment_id INTEGER REFERENCES yh.employment (employment_id)
+    employment_id bigint NOT NULL REFERENCES yh.employment (employment_id),
+    UNIQUE (employment_id)
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.teacher (
     teacher_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    employment_id INTEGER REFERENCES yh.employment (employment_id)
+    employment_id bigint NOT NULL REFERENCES yh.employment (employment_id),
+    UNIQUE (employment_id)
 ) ;
+
+
 
 CREATE TABLE IF NOT EXISTS yh.student (
     student_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    affiliation_id INTEGER REFERENCES yh.affiliation (affiliation_id),
-    program_id INTEGER REFERENCES yh.program (program_id),
-    email_internal VARCHAR
+    affiliation_id bigint NOT NULL REFERENCES yh.affiliation (affiliation_id),
+    program_id bigint NOT NULL REFERENCES yh.program (program_id),
+    email_internal varchar(100) NOT NULL,
+    date_start date NOT NULL,
+    date_end date,
+    UNIQUE (affiliation_id, program_id, date_start)
+) ;
+
+CREATE TABLE IF NOT EXISTS yh.cohort (
+    cohort_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    program_id bigint NOT NULL REFERENCES yh.program (program_id),
+    name varchar(100) NOT NULL,
+    code varchar(100) NOT NULL,
+    date_start date NOT NULL,
+    date_end date,
+    UNIQUE (program_id, code, date_start)
 ) ;
 
 
 
 CREATE TABLE IF NOT EXISTS yh.program_branch (
     program_branch_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    program_id INTEGER REFERENCES yh.program (program_id),
-    branch_id INTEGER REFERENCES yh.branch (branch_id)
+    program_id bigint NOT NULL REFERENCES yh.program (program_id),
+    branch_id bigint NOT NULL REFERENCES yh.branch (branch_id),
+    UNIQUE (program_id, branch_id)
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.module_program (
     module_program_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    module_id INTEGER REFERENCES yh.module (module_id),
-    program_id INTEGER REFERENCES yh.program (program_id)
+    module_id bigint NOT NULL REFERENCES yh.module (module_id),
+    program_id bigint NOT NULL REFERENCES yh.program (program_id),
+    UNIQUE (module_id, program_id)
 ) ;
 
 
 
 CREATE TABLE IF NOT EXISTS yh.course_teacher (
     course_teacher_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    course_id INTEGER REFERENCES yh.course (course_id),
-    teacher_id INTEGER REFERENCES yh.teacher (teacher_id)
+    course_id bigint NOT NULL REFERENCES yh.course (course_id),
+    teacher_id bigint NOT NULL REFERENCES yh.teacher (teacher_id),
+    UNIQUE (course_id, teacher_id)
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.course_student (
     course_student_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    course_id INTEGER REFERENCES yh.course (course_id),
-    student_id INTEGER REFERENCES yh.student (student_id)
+    course_id bigint NOT NULL REFERENCES yh.course (course_id),
+    student_id bigint NOT NULL REFERENCES yh.student (student_id),
+    UNIQUE (course_id, student_id)
 ) ;
 
 
-
-CREATE TABLE IF NOT EXISTS yh.cohort (
-    cohort_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR,
-    code VARCHAR
-) ;
 
 CREATE TABLE IF NOT EXISTS yh.cohort_manager (
     cohort_manager_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    cohort_id INTEGER REFERENCES yh.cohort (cohort_id),
-    manager_id INTEGER REFERENCES yh.manager (manager_id)
+    cohort_id bigint NOT NULL REFERENCES yh.cohort (cohort_id),
+    manager_id bigint NOT NULL REFERENCES yh.manager (manager_id),
+    UNIQUE (cohort_id, manager_id)
 ) ;
 
 CREATE TABLE IF NOT EXISTS yh.student_cohort (
-    student_cohort bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    student_id INTEGER REFERENCES yh.student (student_id),
-    cohort_id INTEGER REFERENCES yh.cohort (cohort_id)
+    student_cohort_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    student_id bigint NOT NULL REFERENCES yh.student (student_id),
+    cohort_id bigint NOT NULL REFERENCES yh.cohort (cohort_id),
+    UNIQUE (student_id, cohort_id)
 ) ;
